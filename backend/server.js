48ecc089935a4fc82f63e92159f3c2ed5d7645eb
Saumya1015 <svsaumyaverma12345@gmail.com>
2026-07-06 13:06:30 +0530

    Fix PORT variable usage

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const connectToDB = require("./src/config/database")
 connectToDB()
 
 const PORT = process.env.PORT || 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000")
 })
